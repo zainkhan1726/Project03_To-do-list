3e5b3814d98f0129e16b35aabc8b622f97b46820
zainkhan1726 <160050990+zainkhan1726@users.noreply.github.com>
2024-04-20 10:39:44 +0500

    initial commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = []
+let condition = true
+
+while (condition)
+    {
+    let todosAction = await inquirer.prompt(
+    [
+        {
+            name: "firstAction",
+            type: "input",
+            message: "What would you like to add more in your todos?",
+        },
+        {
+            name: "secondAction",
+            type: "confirm",
+            message: "Would you like to add more in your todos",
+            default: "true",
+        }
+    ]
+    )
+
+    todos.push(todosAction.firstAction);
+    console.log(todos);
+    condition = todosAction.secondAction;
+    }
